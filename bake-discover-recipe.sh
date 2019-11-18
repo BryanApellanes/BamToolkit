603fec51ac02ee8ae3efd362bbf01ec0a56a1896
Bryan Apellanes <bapellanes@vimly.com>
2019-11-18 13:55:17 -0800

    recipe output

--- bake-discover-recipe.sh
+++ bake-discover-recipe.sh
@@ -20,6 +20,6 @@ OUTPUTRECIPES=./recipes/
 rm -fr ~/.bam/tmp/bake
 dotnet publish $SRCROOT/_tools/bake/bake.csproj -c Release -r $RUNTIME -o ~/.bam/tmp/bake
 
-~/.bam/tmp/bake/bake /discover:$SRCROOT/_tools/ /output:~/.bam/dist/bin /outputRecipe:$OUTPUTRECIPES$RUNTIME-bamtoolkit.json
+~/.bam/tmp/bake/bake /discover:$SRCROOT/_tools/ /output:/tmp/bam/bin /outputRecipe:$OUTPUTRECIPES$RUNTIME-bamtoolkit.json
 
 fi
--- bake-zip-toolkit.sh
+++ bake-zip-toolkit.sh
@@ -5,6 +5,7 @@ source ./get-os-runtime.sh
 
 ./build-bake.sh
 
-OUTPUT=~/.bam/dist/bin
+mkdir -p /tmp/bam/bin
+OUTPUT=/tmp/bam/bin
 printf "OUTPUT = $OUTPUT\r\n"
 ~/.bam/bake/bake /zip /zipRecipe:./recipes/$RUNTIME-bamtoolkit.json /output:$OUTPUT
