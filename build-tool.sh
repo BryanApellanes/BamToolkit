f27f852269938ddc6af351bda5da3f4146bea7ff
Bryan Apellanes <bapellanes@vimly.com>
2019-11-27 08:36:39 -0800

    build tool help

--- build-tool.sh
+++ build-tool.sh
@@ -4,7 +4,7 @@ if [[ -z "$1" ]] || [[ $1 = "-help" ]] || [[ $1 = "-?" ]] || [[ $1 = "-h" ]]
 then
     printf "usage: build.sh\r\n"
     printf "\r\n"
-    printf "Build the bam toolkit found at the root source directory specified in the file _srcroot or the default of `pwd`/Bam.Net.Core.\r\n"
+    printf "Build the bam tool found at the root source directory specified in the file _srcroot or the default of `pwd`/Bam.Net.Core.\r\n"
     printf "\r\n"
     exit 0
 else
