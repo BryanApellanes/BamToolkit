cdd55dc9853f6e362663f6078ac47c7c741aeea7
Bryan Apellanes <bryan@threeheadz.com>
2020-01-15 14:26:11 -0800

    updated

--- set-keys.sh
+++ set-keys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh
-cp -R ./docker/_ssh/ ~/.ssh 
-chmod 700 ~/.ssh
+cp ./docker/_ssh/id_rsa ~/.ssh/id_rsa
+cp ./docker/_ssh/id_rsa ~/.ssh/id_rsa.pub
+chmod 700 ~/.ssh/id_rsa.pub
 chmod 644 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
