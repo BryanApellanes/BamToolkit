462cb87ac3f3a50007ef9ceb11db826160191e8a
Bryan Apellanes <bryan@threeheadz.com>
2020-01-15 10:38:03 -0800

    ssh perms

--- ci-commit-test.sh
+++ ci-commit-test.sh
@@ -24,4 +24,8 @@ mkdir -p ~/.ssh
 echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
 echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
+chmod 700 ~/.ssh
+chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
+
 git push "git@github.com:BryanApellanes/BamToolkit.git" HEAD:ci-test -f
