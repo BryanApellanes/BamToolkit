90e78f7bea9554d3a70ddfa8b8d07961f1092fdb
Bryan Apellanes <bryan@threeheadz.com>
2020-02-15 07:45:24 -0800

    build scripts

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 = "-help" ]] || [[ $1 = "-?" ]] || [[ $1 = "-h" ]]; then
-    printf "usage: docker-clean.sh <image-name>\r\n"
+    printf "usage: clean.sh <image-name>\r\n"
     printf "\r\n"
     printf "Using docker, deletes exited containers and dangling images."
     printf "\r\n"
