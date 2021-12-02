c69ac541586c161e973ee2daa7a07d4ce93a6fa6
Bryan Apellanes <bryan.apellanes@okta.com>
2021-12-02 07:01:12 -0800

    delete everything

--- run-unit-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -z $TESTBIN ]]; then
-    TESTBIN=/tmp/bam/tests
-fi
-
-bamtest /dir:${TESTBIN} /search:*Tests.* /type:Unit /data:/root/.bam/tests/results 
--- set-keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.ssh
-cp ./docker/_ssh/id_rsa ~/.ssh/id_rsa
-cp ./docker/_ssh/id_rsa.pub ~/.ssh/id_rsa.pub
-
-chmod 600 ~/.ssh/id_rsa
-chmod 644 ~/.ssh/id_rsa.pub
